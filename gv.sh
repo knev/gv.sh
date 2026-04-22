58fab8b804c970bbf57871d6251c12dbf1c2025e
knev <knev@users.noreply.github.com>
2026-04-22 13:20:24 +0200

    fix: remove --java for now.

--- gv.sh
+++ gv.sh
@@ -6,7 +6,7 @@
 # PWD=$(cd "$(dirname "$0")" && pwd)
 
 usage() {
-	echo "usage: $(basename $0) [-a] [--js [PATH]|--java|--vs [PATH]] [--nsi [PATH]] [--antora [PATH]] [--agv [--fix]] [--tag TAG] [-h | --help]"
+	echo "usage: $(basename $0) [-a] [--js [PATH]|--vs [PATH]] [--nsi [PATH]] [--antora [PATH]] [--agv [--fix]] [--tag TAG] [-h | --help]"
 	echo
 	echo "Examples:"
 	echo "    gv --js --tag api                  // package.json=  \"version\": \"0.0.21-api.5\" "
